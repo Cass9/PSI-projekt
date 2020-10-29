48f11227dd2582bb3a8857b2b7ca36f10edb5ea6
cass99 <56155738+cass99@users.noreply.github.com>
2020-10-29 12:04:05 +0100

    Zrobione 1 i 2 z ćwiczeń

---
+++ 01-WdP/cw01.py
@@ -0,0 +1,15 @@
+lorem = "Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker"
+
+imie = "Piotr"
+nazwisko = "Mierzejewski"
+
+litera1 = imie[2]
+litera2 = nazwisko[3]
+
+print(litera1)
+print(litera2)
+
+liczba_liter1 = lorem.count(litera1)
+liczba_liter2 = lorem.count(litera2)
+
+print("W tekście jest %i liter %s i %i liter %s" % (liczba_liter1, litera1, liczba_liter2, litera2))
