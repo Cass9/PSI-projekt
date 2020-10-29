9b1eef73d6961aa68cf2fdfdfca11c4d277a9f73
cass99 <56155738+cass99@users.noreply.github.com>
2020-10-29 12:20:08 +0100

    Podpunkt 3 z cw1

---
+++ 01-WdP/cw01-3.py
@@ -0,0 +1,12 @@
+print("Coś robie {} końca {} wiem {}".format('do', 'nie', 'co'))
+print("{0}, {0}, {1}, {0}".format('nie wiem', 'nic'))
+
+liczby = (1, 2, 3)
+print("X:{0[0]} Y:{0[1]} Z:{0[2]}".format(liczby))
+
+zdobyte_punkty = 10
+max_punkty = 20
+
+print("Procent poprawnych odpowiedzi: {:.2%}".format(zdobyte_punkty/max_punkty))
+print("Liczba w innym formacie: {:,}".format(382728374))
+
