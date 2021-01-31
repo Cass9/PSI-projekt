d9281e32b420a000d2715c8f328ccb0818256fab
Marek <kontakt.maras98@gmail.com>
2021-01-31 19:30:52 +0100

    dodano deugi test

--- wypozyczalnia_samochodow/wypozyczalnia_samochodow/firma_zlomek/tests.py
+++ wypozyczalnia_samochodow/wypozyczalnia_samochodow/firma_zlomek/tests.py
@@ -19,6 +19,26 @@ class AutoTests(APITestCase):
         response = self.post_Auto(nowa_marka)
         assert response.status_code == status.HTTP_201_CREATED
         assert Auto.objects.count() == 1
-         assert Auto.objects.get().name == nowa_marka   
+        assert Auto.objects.get().nowa_marka == nowa_marka   
 
-class 
+class KlienTest(APITestCase):
+    def post_Klient(self, Imie, Nazwisko, PESEL, numer_dowodu_osobistego, Miejscowosc):
+        url = reverse(views.KlientList.name)
+        data = {'imie':Imie, 'nazwisko':Nazwisko, 'PESEL':PESEL, 'numer_dowodu_osobistego':numer_dowodu_osobistego, 'miejscowosc':Miejscowosc}
+        response = self.client.post(url, data, format='json')
+        return response
+
+    def test_post_and_get_klient(self):
+        nowe_imie='Kamil'
+        nowe_nazwisko='Wytry'
+        nowy_PESEL='98080804182'
+        nowy_numer_dowodu_osobistego='CDF2019232'
+        nowa_miejscowosc='Bielawa'
+        response = self.post_Klient(nowe_imie, nowe_nazwisko, nowy_PESEL, nowy_numer_dowodu_osobistego, nowa_miejscowosc)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert Klient.objects.count() == 2
+        assert Klient.objects.get().imie == nowe_imie
+        assert Klient.objects.get().nazwisko == nowe_nazwisko
+        assert Klient.objects.get().PESEL == nowy_PESEL
+        assert Klient.objects.get().numer_dowodu_osobistego == nowy_numer_dowodu_osobistego
+        assert Klient.objects.get().nowa_miejscowosc == nowa_miejscowosc  
